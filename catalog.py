a82c8ae01fcc3d44f554d90d7a14d07786ab957f
jbaik318 <jbaik318@gmail.com>
2017-12-08 13:21:34 -0700

    catalog.py - add functions that allow editing and deletion of item 		   - add functions that allows retrival of database of catalog and item

--- catalog.py
+++ catalog.py
@@ -10,6 +10,33 @@ Base.metadata.bind = engine
 DBSession =  sessionmaker(bind = engine)
 session = DBSession()
 
+
+def categoryToId(category_name):
+	iso = session.query(Category).filter_by(name  = category_name.title())
+	catId = ""
+	for x in iso:
+		catId = x.id
+	category = session.query(Category).filter_by(id = catId).one()
+	return category
+
+def itemToId(item_name):
+	iso = session.query(Item).filter_by(name = item_name)
+	itemId = ""
+	for x in iso:
+		itemId =  x.id
+	item = session.query(Item).filter_by(id = itemId).first()
+	return item
+
+def categoryFromItem(item_name):
+	iso = session.query(Item).filter_by(name = item_name)
+	itemId = ""
+	for x in iso:
+		itemId = x.id
+	category = session.query(Category).filter_by(id = itemId).first()
+	return category
+
+
+
 #routing endpoints
 @app.route('/')
 def showCatalog():
@@ -17,49 +44,58 @@ def showCatalog():
 	listItem = session.query(Item).all()
 	return render_template('index.html', catalog = listCatalog, item = listItem)
 	
-@app.route('/<category_name>')
-@app.route('/<category_name>/items')
+@app.route('/catalog/<category_name>')
+@app.route('/catalog/<category_name>/items')
 def showCategory(category_name):	
 	listCatalog = session.query(Category).all()
-	iso = session.query(Category).filter_by(name  = category_name.title())
-	for x in iso:
-		catId = x.id
-	category = session.query(Category).filter_by(id = catId).one()
+	category = categoryToId(category_name)
 	item = session.query(Item).filter_by(category_id = category.id)
 
 	return render_template('viewCategory.html', catalog = listCatalog, category = category, item = item)
 
-
-@app.route('/<string:category>/<string:item>')
-def showItem(category, item):
-
-	return ('this page shows details on each item')
-
-@app.route('/<string:category>/create', methods = ['GET','POST'])
-def createItem(category):
-	specificCategory = session.query(Category).filter_by(name = category.title())
-	category = session.query(Category).filter_by(id = specificCategory[0].id)
-	
+@app.route('/catalog/<category_name>/create', methods = ['GET','POST'])
+def createItem(category_name):
+	category = categoryToId(category_name)
 	if request.method == 'POST':
-		addItem = Item(name = request.form['name'], description = request.form['description'], menu_id = category.id)
+		addItem = Item(name = request.form['name'], category_id = category.id)
 		session.add(addItem)
 		session.commit()
-		return redirect(url_for('showCategory'))
+		return redirect(url_for('showCategory', category_name = category.name))
 	else:
-		return render_template('newCategory.html')
+		return render_template('newCategory.html', category = category)
 
-@app.route('/<string:category>/edit', methods = ['GET','POST'])
-def editItem(category):
+@app.route('/catalog/<item_name>/edit', methods = ['GET','POST'])
+def editItem(item_name):
+	item = itemToId(item_name)
+	category = session.query(Category).filter_by(id = item.category_id).first()
+	if request.method ==  'POST':
+		item.name = request.form['name']
+		session.add(item)
+		session.commit()
+		return redirect(url_for('showCategory', category_name = category.name))
+	else:
+		return render_template('editItem.html', item = item)
+	
 
-	return ('this page edits item')
+@app.route('/catalog/<item_name>/delete', methods = ['GET','POST'])
+def deleteItem(item_name):
+	item = itemToId(item_name)
+	category = session.query(Category).filter_by(id = item.category_id).first()
+	if request.method == 'POST':
+		session.delete(item)
+		session.commit()
+		return redirect(url_for('showCategory', category_name = category.name))
+	else:
+		return render_template('deleteItem.html', item = item )
 
-@app.route('/<string:category>/delete')
-def deleteItem(category):
-	return ('this page deletes item')
+'''@app.route('/<string:category_name>/<string:item_name>')
+def showItem(category_name, item_name):
+	category = categoryToId(category_name)
 
+	return ('this page shows details on each item')'''
 
 #designate port to operate
 if __name__ == '__main__':
   	app.secret_key = 'super_secret_key'
 	app.debug = True
-  	app.run(host = '0.0.0.0', port = 5000)
+  	app.run(host = '0.0.0.0', port = 8000)
