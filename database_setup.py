637493219fbfab171776bbb3f2150d24be46b928
jbaik318 <jbaik318@gmail.com>
2017-12-12 16:52:59 -0700

    catalog.py - add authorization functions using google oauth client_secrets.json - contains all id's regardin the user database_setup.py - add additional rows for user that accounts to google oauth index.

--- database_setup.py
+++ database_setup.py
@@ -14,16 +14,20 @@ class Item(Base):
 	__tablename__ = 'item'
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 	#description = Column(String(250))
 	#catType = Column(String(80))
 	category_id = Column(Integer, ForeignKey('category.id'))
 	category = relationship(Category)
 
-class User:
-	__tablename__ = 'user'
-	name = Column(String(80), index = False)
-	id = Column(Integer, primary_key = True)
-	#add password hases here
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 engine = create_engine('sqlite:///catalog.db')
 
